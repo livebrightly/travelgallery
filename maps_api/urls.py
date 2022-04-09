23c4293620f809a7e95c46346c9f0106f6771826
Johnny C <cheungjc@outlook.com>
2022-04-09 18:06:46 -0400

    added models and updated templates

--- maps_api/urls.py
+++ maps_api/urls.py
@@ -1,17 +1,22 @@
+from django.shortcuts import render
 from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+
 # basic string HTTP response
 urlpatterns = [
     path('', views.default_map, name="default"),
+    path('maps/', views.maps_index, name='index'),
+    path('about/', views.about, name="about"),
+    path('/<int:map_id>/add_photo/', views.add_photo, name='add_photo')
 ]
 
 
 # NOTE from map api docs
-# from django.conf.urls import url                                                                                                                              
+# from django.conf.urls import url
 # from . import views
 
-# urlpatterns = [ 
+# urlpatterns = [
 #     url(r'', views.default_map, name="default"),
 # ]
