5bc65dd5569f62f5981a8d12aafa6ecdf2460257
Johnny C <cheungjc@outlook.com>
2022-04-11 23:19:30 -0400

    updated main_app models and create function

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# import your models here
+from .models import Image
 
-# Register your models here.
+# Register your models here
+# admin.site.register(Map)
+admin.site.register(Image)
